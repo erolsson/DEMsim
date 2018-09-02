e20265bfdde2a17d8296071c34b55d19765fe5d0
erolsson <erolsson@kth.se>
2018-09-02 17:29:40 +0200

    First version

--- source/engine.h
+++ source/engine.h
@@ -10,6 +10,7 @@
 #include <map>
 #include <vector>
 
+#include "collision_detector.h"
 #include "contact.h"
 #include "contact_matrix.h"
 #include "cylinder.h"
@@ -25,14 +26,18 @@ namespace DEM {
         using ParticlePointer = ParticleType*;
         using PointSurfacePointer = PointSurface<ForceModel, ParticleType>*;
         using CylinderPointer = Cylinder<ForceModel, ParticleType>*;
+
         Engine();
 
         //Object creation functions
         template<typename MaterialType>
         MaterialType* create_material(double density);
+
         ParticlePointer create_particle(double radius, const Vec3& position, const Vec3& velocity,
                                       MaterialBase* material);
+
         PointSurfacePointer create_point_surface(const std::vector<Vec3>& points, bool infinite);
+
         CylinderPointer create_cylinder(double radius, const Vec3& axis, const Vec3& base_point, double length,
                                         bool inward=true, bool infinite=false);
 
@@ -40,18 +45,18 @@ namespace DEM {
         using ContactType = Contact<ForceModel, ParticleType>;
         using SurfaceType = Surface<ForceModel, ParticleType>;
 
-        std::size_t number_of_objects_;
-        std::vector<MaterialBase*> materials_;
-        std::vector<ParticleType*> particles_;
-        std::vector<SurfaceType*> surfaces_;
-        ContactMatrix<ContactType> contacts_;
+        std::size_t number_of_objects_{ 0 };
+        std::vector<MaterialBase*> materials_{};
+        std::vector<ParticleType*> particles_{};
+        std::vector<SurfaceType*> surfaces_{};
+        ContactMatrix<ContactType> contacts_{};
 
+        CollisionDetector<ForceModel, ParticleType> collision_detector_;
     };
 
     template<typename ForceModel, typename ParticleType>
     Engine<ForceModel, ParticleType>::Engine() :
-        number_of_objects_(0),
-        particles_()
+        collision_detector_(particles_, surfaces_, contacts_)
     {
         // Empty constructor
     }
