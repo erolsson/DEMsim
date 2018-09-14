bc830cc3d44f741a637ef824a4306f8b003a19d3
erolsson <erolsson@kth.se>
2018-09-14 19:37:59 +0200

    First version

--- source/filling_functions.cpp
+++ source/filling_functions.cpp
@@ -6,19 +6,20 @@
 
 #include <random>
 
-std::vector<DEM::Vec3> DEM::randomF_fill_cylinder(double z0, double z1, double R, std::vector<double> radii)
+std::vector<DEM::Vec3> DEM::random_fill_cylinder(double z0, double z1, double R, std::vector<double> radii)
 {
     std::vector<Vec3> particle_positions;
+    std::random_device random_device;
+    std::default_random_engine rand_endine(random_device());
+    std::uniform_real_distribution<double> dist_r(-R, R);
+    std::uniform_real_distribution<double> dist_z(z0, z1);
     for (auto particle_radius : radii) {
         bool overlapping = true;
         Vec3 position {};
         while(overlapping){
-            double r = double(rand())/(double(RAND_MAX)+1.0);
-            position.x = -R + particle_radius + 2*r*(R-particle_radius);
-            r = double(rand())/(double(RAND_MAX)+1.0);
-            position.y = -R + particle_radius + 2*r*(R-particle_radius);
-            r = double(rand())/(double(RAND_MAX)+1.0);
-            position.z = z0 + particle_radius + r*(z1-2*particle_radius);
+            position.x = dist_r(rand_endine);
+            position.y = dist_r(rand_endine);
+            position.z = dist_z(rand_endine);
 
             //Check if a particle at the chosen position overlaps with an other
             if (position.x*position.x+position.y*position.y < (R-particle_radius)*(R-particle_radius)) {
