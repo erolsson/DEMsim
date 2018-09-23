2603ab3b1f93d544d3cd2319d2a6de62ed6f4d24
erolsson <erolsson@kth.se>
2018-09-23 21:47:12 +0200

    "Visualization of particles"

---
+++ post_processing/particle_vizulization.py
@@ -0,0 +1,31 @@
+import numpy as np
+from mayavi import mlab
+
+
+class SpheresPlotter:
+    def __init__(self):
+        self.ms = None
+
+    def plot(self, data, color=(184./255, 115./255., 51./255.)):
+        x = data[:, 1]
+        y = data[:, 2]
+        z = data[:, 3]
+        R = data[:, 4]
+        if self.ms is None:
+            self.ms = mlab.points3d(x, y, z, 2 * R,
+                                    color=color,
+                                    resolution=32,
+                                    scale_factor=1., scale_mode='scalar').mlab_source
+        else:
+            self.ms.set(x=x, y=y, z=z)
+            
+
+if __name__ == '__main__':
+    simulation_directory = '../results/gyratory_compaction/1/'
+    time = 0.01
+    
+    mlab.figure(size=(1920, 1200))
+    plotter = SpheresPlotter()
+    particle_file_name = 'particles_' + str(time) + '.dat'
+    
+    particle_data = np.genfromtxt(particle_file_name, delimiter=',')
