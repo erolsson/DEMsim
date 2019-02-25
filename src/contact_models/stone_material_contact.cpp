0850dc02bdbc9b8bf213bce0f43e9f1f7d5829c5
erolsson <erolsson@kth.se>
2019-02-25 09:57:04 +0100

    Implementing stone-materials

--- src/contact_models/stone_material_contact.cpp
+++ src/contact_models/stone_material_contact.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "stone_material_contact.h"
+
+DEM::StoneMaterialContact::StoneMaterialContact(DEM::StoneMaterialContact::ParticleType* particle1,
+                                                DEM::StoneMaterialContact::ParticleType* particle2,
+                                                std::chrono::duration<double>)
+{
+
+}
+
+DEM::StoneMaterialContact::StoneMaterialContact(DEM::StoneMaterialContact::ParticleType* particle1,
+                                                DEM::StoneMaterialContact::SurfaceType* surface,
+                                                std::chrono::duration<double>)
+{
+
+}
+
+void DEM::StoneMaterialContact::update(double h, const DEM::Vec3& dt, const DEM::Vec3& normal)
+{
+    update_normal_force(h);
+    update_tangential_force(dt, normal);
+}
+
+void DEM::StoneMaterialContact::update_normal_force(double h)
+{
+    if (h > 0) {
+        if (h >= hmax_) {
+            F_ = kp_*pow(h, 1.5);
+            hmax_ = h;
+            hp_ = (hmax_ - pow(F_/ke_, 1./1.5)); // Plastic indentation depth
+            ku_ = F_/pow(h-hp_, unloading_exp_);
+        }
+        a_ = sqrt(h*R0_);
+        else if (h > hp_) {
+            if (h > h_) {
+                F_ = kl_*pow(h-hl_, 1.5);
+                ku_ = F_/pow(h-hp_, unloading_exp_);
+            }
+            else {
+                F_ = ku_*pow(h-hp_, unloading_exp_);
+                double A = pow(F_/kp_, 1./1.5)/hmax_;
+                hl_ = (h - A*hmax_)/(1-A);
+                kl_ = kp_*pow(hmax_/(hmax_-hl_), 1.5);
+            }
+        }
+        else {
+            F_ = 0.;
+            a_ = 0;
+            hl_ = hp_;
+            kl_ = ke_;
+        }
+    }
+    else {
+        F_ = 0.;
+        a_ = 0;
+        hl_ = hp_;
+        kl_ = ke_;
+    }
+    h_ = h;
+}
+
+void DEM::StoneMaterialContact::update_tangential_force(const DEM::Vec3& dt, const DEM::Vec3& normal)
+{
+
+}
+
--- src/contact_models/stone_material_contact.h
+++ src/contact_models/stone_material_contact.h
@@ -5,8 +5,48 @@
 #ifndef DEMSIM_STONE_MATERIAL_CONTACT_H
 #define DEMSIM_STONE_MATERIAL_CONTACT_H
 
-class StoneMaterialContact {
+#include "../particles/fractureable_spherical_particle.h"
+#include "../utilities/vec3.h"
 
-};
+namespace DEM {
+    class StoneMaterialContact {
+        using ParticleType = FractureableSphericalParticle<StoneMaterialContact>;
+        using SurfaceType = Surface<StoneMaterialContact, ParticleType>;
+
+    public:
+        StoneMaterialContact(ParticleType* particle1, ParticleType* particle2, std::chrono::duration<double>);
+        StoneMaterialContact(ParticleType* particle1, SurfaceType* surface, std::chrono::duration<double>);
+        void update(double h, const Vec3& dt, const Vec3& normal);
+        double get_overlap() const { return h_; }
+        double get_normal_force() const { return F_; }
+        const Vec3& get_tangential_force() const { return FT_; }
+        double get_contact_area() const { return sqrt(a_); }
+        bool active() const { return F_!=0; }
+        void set_increment(std::chrono::duration<double>) { }
+
+    private:
+        void update_normal_force(double h);
+        void update_tangential_force(const Vec3& dt, const Vec3& normal);
+
+        double h_ { 0. };
+        double hmax_ { 0. };
+        double hp_ { 0. };
+        double hl_ {0. };
+
+        double F_ { 0. };
+        double a_ { 0, };
+        double R0_;
+
+        Vec3 FT_ {Vec3(0., 0., 0.)};
+
+        // Material parameters for the stone contact model
+        double kp_;           //Loading stiffness
+        double ke_;
+        double kl_;
+        double ku_;           //Unloading stiffness
+
+        double unloading_exp_;
+    };
+}
 
 #endif //DEMSIM_STONE_MATERIAL_CONTACT_H
