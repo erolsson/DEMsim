56a6246c6dddbd36ca5b0afc2aec0b6145e54ac9
erolsson@kth.se <erolsson@kth.se>
2019-08-20 21:56:51 +0200

    "Updated optimization flag to -O3"

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main(int argc, char** argv)
 {
-   //feenableexcept(FE_INVALID | FE_OVERFLOW);
+    feenableexcept(FE_INVALID | FE_OVERFLOW);
     if (argc < 2 || DEM::valid_simulations().count(argv[1]) == 0) {
         std::cerr << "Please provide a valid program name as first argument" << '\n';
         return 0;
