fca10a4093d912dd93ff86a9f06d995fd7c8c8c6
elahe <etemadi@kth.se>
2020-04-02 10:22:16 +0200

    "Changed file extension to .dou in python-files"

--- src/simulations/electrode_box.cpp
+++ src/simulations/electrode_box.cpp
@@ -142,7 +142,7 @@ void DEM::electrode_box(const std::string &settings_file_name) {
 
 
     std::cout<<"beginning of unloading"<< std::endl;
-    top_surface->set_velocity(Vec3(0, 0, surface_velocity));
+    top_surface->set_velocity(Vec3(0, 0, surface_velocity*10));
     simulator.run(max_velocity);
 
 
@@ -163,7 +163,7 @@ void DEM::electrode_box(const std::string &settings_file_name) {
     //top_surface->move(-Vec3(0,0,points_[1].z()-h),Vec3(0,0,0));
     //std::vector<Vec3> points_=top_surface->get_points();
     std::cout<<"Moving the side surface to get force-deformation "<< std::endl;
-    double side_surface_velocity=0.05;
+    double side_surface_velocity=0.075;
     side_surface_2->set_velocity(Vec3(side_surface_velocity-0. , 0, 0.));
     //double delta_=points_[1].z()*1.6/100.0;
     std::chrono::duration<double> side_surface_time {((0.0240) / surface_velocity)};
