b13936b255706d7adc786ca5e2026ebecc8470b1
elahe <etemadi@kth.se>
2020-04-27 11:32:30 +0200

    "C1"

--- src/simulations/electrode_box.cpp
+++ src/simulations/electrode_box.cpp
@@ -144,7 +144,8 @@ void DEM::electrode_box(const std::string &settings_file_name) {
 
     std::cout<<"beginning of unloading"<< std::endl;
     top_surface->set_velocity(Vec3(0, 0, surface_velocity));
-    simulator.run(max_velocity);
+    run_for_time.reset(compaction_time/9.0);
+    simulator.run(run_for_time);
 
 
     std::cout<<"Calculation Porosity"<< std::endl;
