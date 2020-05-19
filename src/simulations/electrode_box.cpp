7cd00aac7dd6bbcf2aef168de61487f754bc5348
elahe <etemadi@kth.se>
2020-05-19 10:50:01 +0200

    "bt1.5"

--- src/simulations/electrode_box.cpp
+++ src/simulations/electrode_box.cpp
@@ -133,9 +133,10 @@ void DEM::electrode_box(const std::string &settings_file_name) {
     auto bbox = simulator.get_bounding_box();
     double h = bbox[5];
     std::cout<<"h"<< h << std::endl;
+
     top_surface->move(-Vec3(0, 0, box_height - h), Vec3(0, 0, 0));
     std::cout<<"h"<< h<< std::endl;
-    double surface_velocity = 2e-03;
+    double surface_velocity = 2e-04;
     top_surface->set_velocity(Vec3(0, 0, 0.-surface_velocity));
     //std::chrono::duration<double> compaction_time {((h - mat->active_particle_height) / surface_velocity)};
     //run_for_time.reset(compaction_time);
