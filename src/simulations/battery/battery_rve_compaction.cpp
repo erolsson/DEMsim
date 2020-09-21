b2b1b7cbf0943ebe027f2f4681da6e6431d63b2a
erolsson@kth.se <erolsson@kth.se>
2020-09-21 10:25:20 +0200

    "Implementing deformable surfaces"

--- src/simulations/battery/battery_rve_compaction.cpp
+++ src/simulations/battery/battery_rve_compaction.cpp
@@ -2,3 +2,8 @@
 // Created by erolsson on 21/09/2020.
 //
 
+#include "../simulations.h"
+void DEM::battery_rve_compaction(const std::string& settings_file_name)
+{
+
+}
--- src/simulations/battery/battery_rve_loading.cpp
+++ src/simulations/battery/battery_rve_loading.cpp
@@ -2,3 +2,8 @@
 // Created by erolsson on 21/09/2020.
 //
 
+#include "../simulations.h"
+
+void DEM::battery_rve_loading(const std::string& settings_file_name) {
+
+}
