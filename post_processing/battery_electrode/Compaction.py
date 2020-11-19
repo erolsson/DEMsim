257b869a7454f2b6bbca484f7c37d31da7c76de3
elahe <etemadi@kth.se>
2020-11-19 15:23:10 +0100

    "Testing"

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -49,35 +49,19 @@ def volume_cylinder(data_directory):
     return volume
 
 
-def relative_density_cylinder(data_directory):
-    data = volume_cylinder(data_directory)
-    data[:, 1] = particle_volume(data_directory) / volume_cylinder(data_directory)[:, 1]
-    return data
-
-
 def pressures_cylinder(data_directory):
-    dimensions = dimensions_box(data_directory)
     force_data = np.genfromtxt(data_directory + '/surface_forces.dou', delimiter=', ')
-    r = dimensions[:, 1]
-    z0 = dimensions[:, 2]
-    z1 = dimensions[:, 3]
-
     with open(data_directory + '/surface_positions.dou', 'r') as position_data_file:
         first_line = position_data_file.readlines()[0]
         first_line = first_line.split(', ')
         id_idx = [i for i in range(len(first_line)) if first_line[i].startswith('ID')]
     surface_types = [first_line[idx+1][5:] for idx in id_idx]
 
-    cyl_idx = [i for i, surface_type in enumerate(surface_types) if surface_type == 'Cylinder'][0]
     surface_indices = [i for i, surface_type in enumerate(surface_types) if surface_type == 'PointSurface']
-    cylinder_force = force_data[:, cyl_idx*5+1]
-    surface1_force = force_data[:, surface_indices[0]*5+1]
-    surface2_force = force_data[:, surface_indices[1]*5+1]
 
-    p = 1*dimensions
-    p[:, 1] = cylinder_force/(2*pi*r*np.abs(z0-z1))
-    p[:, 2] = surface1_force/(pi*r**2)
-    p[:, 3] = surface2_force/(pi*r**2)
+    surface1_force = force_data[:, surface_indices[0]*5+1]
+
+    p = surface1_force/(0.34*0.34)
     return p
 
 
@@ -85,8 +69,7 @@ if __name__ == '__main__':
     simulation_directory = '../results/viscoelastic/11-18-kvall/'
     v_particles = particle_volume(simulation_directory)
     for directory in ["", "unload_D=0.65", "unload_D=0.7", "unload_D=0.75", "unload_D=0.8"]:
-        v_cylinder = volume_cylinder(simulation_directory + directory)
+        porosity = (1-particle_volume(simulation_directory+directory)/volume_cylinder(simulation_directory+directory))
         pressures = pressures_cylinder(simulation_directory + directory)
-        relative_density = v_particles/v_cylinder
-        plt.plot(relative_density, pressures[:, 1:])
+        plt.plot(porosity*100, pressures[:, 1:])
     plt.show()
