9da45924d8a2e6908b7243b0d10830ee1c6a4d27
elahe <etemadi@kth.se>
2020-11-22 17:41:44 +0100

    "Testing"

--- src/simulations/electrode_box.cpp
+++ src/simulations/electrode_box.cpp
@@ -29,8 +29,8 @@ void DEM::electrode_box(const std::string& settings_file_name) {
     //mat->kT = parameters.get_parameter<double>("kT");
     mat->Ep = parameters.get_parameter<double>("Ep");
     mat->nu = parameters.get_parameter<double>("nu");
-    mat->fraction_binder_contacts= parameters.get_parameter<double>("fraction_binder_contacts");
-    mat->binder_radius_fraction=parameters.get_parameter<double>("binder_radius_fraction");
+    mat->fraction_binder_contacts = parameters.get_parameter<double>("fraction_binder_contacts");
+    mat->binder_radius_fraction = parameters.get_parameter<double>("binder_radius_fraction");
     mat->nup = parameters.get_parameter<double>("nup");
     mat->mu = parameters.get_parameter<double>("mu");
     mat->mu_wall = parameters.get_parameter<double>("mu_wall");
@@ -44,30 +44,30 @@ void DEM::electrode_box(const std::string& settings_file_name) {
 
 
     auto particle_radii = read_vector_from_file<double>(particle_file);
-    particle_radii.assign(particle_radii.begin(), particle_radii.begin()+N);
+    particle_radii.assign(particle_radii.begin(), particle_radii.begin() + N);
     std::sort(particle_radii.rbegin(), particle_radii.rend());
     double particle_volume = 0.;
-    for(const auto& r: particle_radii) {
-        particle_volume += 4.*pi*r*r*r/3.;
+    for (const auto &r: particle_radii) {
+        particle_volume += 4. * pi * r * r * r / 3.;
     }
     std::cout << "Volume of simulated particles is " << particle_volume << "\n";
-    auto box_side = pow(particle_volume/particle_density_at_cube, 1./3);
-    std::cout << "box_side " <<box_side << "\n";
-    auto box_height = particle_density_at_cube*box_side/particle_density_at_filling;
-    std::cout << "box_height " <<box_height << "\n";
+    auto box_side = pow(particle_volume / particle_density_at_cube, 1. / 3);
+    std::cout << "box_side " << box_side << "\n";
+    auto box_height = particle_density_at_cube * box_side / particle_density_at_filling;
+    std::cout << "box_height " << box_height << "\n";
 
-    auto p1 = Vec3(-box_side/2, -box_side/2, 0);
-    auto p2 = Vec3( box_side/2, -box_side/2, 0);
-    auto p3 = Vec3( box_side/2,  box_side/2, 0);
-    auto p4 = Vec3(-box_side/2,  box_side/2, 0);
-    auto p5 = Vec3(-box_side/2, -box_side/2, box_height);
-    auto p6 = Vec3( box_side/2, -box_side/2, box_height);
-    auto p7 = Vec3( box_side/2,  box_side/2, box_height);
-    auto p8 = Vec3(-box_side/2,  box_side/2, box_height);
+    auto p1 = Vec3(-box_side / 2, -box_side / 2, 0);
+    auto p2 = Vec3(box_side / 2, -box_side / 2, 0);
+    auto p3 = Vec3(box_side / 2, box_side / 2, 0);
+    auto p4 = Vec3(-box_side / 2, box_side / 2, 0);
+    auto p5 = Vec3(-box_side / 2, -box_side / 2, box_height);
+    auto p6 = Vec3(box_side / 2, -box_side / 2, box_height);
+    auto p7 = Vec3(box_side / 2, box_side / 2, box_height);
+    auto p8 = Vec3(-box_side / 2, box_side / 2, box_height);
     std::vector<Vec3> bottom_points{p1, p2, p3, p4};
     std::vector<Vec3> top_points{p8, p7, p6, p5};
 
-    auto particle_positions = random_fill_box(-box_side/2, box_side/2, -box_side/2, box_side/2,
+    auto particle_positions = random_fill_box(-box_side / 2, box_side / 2, -box_side / 2, box_side / 2,
                                               0, box_height, particle_radii, mat->bt);
 
     auto deformable_surface = simulator.create_deformable_point_surface(bottom_points, true);
@@ -76,9 +76,9 @@ void DEM::electrode_box(const std::string& settings_file_name) {
     std::cout << "Normal of top surface: " << top_surface->get_normal() << "\n";
 
     for (std::size_t i = 0; i != particle_positions.size(); ++i) {
-        simulator.create_particle(particle_radii[i], particle_positions[i], Vec3(0,0,0), mat);
+        simulator.create_particle(particle_radii[i], particle_positions[i], Vec3(0, 0, 0), mat);
     }
-    auto filling_output = simulator.create_output(output_directory , 0.001s);
+    auto filling_output = simulator.create_output(output_directory, 0.001s);
 
     filling_output->print_particles = true;
     filling_output->print_kinetic_energy = true;
@@ -87,36 +87,37 @@ void DEM::electrode_box(const std::string& settings_file_name) {
     filling_output->print_contacts = true;
     filling_output->print_periodic_bc = true;
     filling_output->print_mirror_particles = true;
-    filling_output->print_fabric_force_tensor=true;
+    filling_output->print_fabric_force_tensor = true;
 
 
-    simulator.add_periodic_boundary_condition('x', -box_side/2, box_side/2);
-    simulator.add_periodic_boundary_condition('y', -box_side/2, box_side/2);
+    simulator.add_periodic_boundary_condition('x', -box_side / 2, box_side / 2);
+    simulator.add_periodic_boundary_condition('y', -box_side / 2, box_side / 2);
 
     simulator.set_gravity(Vec3(0, 0, -9.82));
     simulator.set_mass_scale_factor(10.0);
-    simulator.setup(1.01*mat->bt);
+    simulator.setup(1.01 * mat->bt);
     EngineType::RunForTime run_for_time(simulator, 0.1s);
     simulator.run(run_for_time);
-    EngineType::ParticleVelocityLess max_velocity (simulator, 0.1, 0.01s);
+    EngineType::ParticleVelocityLess max_velocity(simulator, 0.1, 0.01s);
     simulator.run(max_velocity);
 
     // Move the lid to the uppermost particle
-    std::cout<<"beginning of compaction"<< std::endl;
+    std::cout << "beginning of compaction" << std::endl;
     auto bbox = simulator.get_bounding_box();
     double h = bbox[5];
-    top_surface->move(-Vec3(0, 0, box_height - h-1.1*mat->bt), Vec3(0, 0, 0));
-    std::cout<<"h"<< h<< std::endl;
+    top_surface->move(-Vec3(0, 0, box_height - h - 1.1 * mat->bt), Vec3(0, 0, 0));
+    std::cout << "h" << h << std::endl;
     double surface_velocity = 0.01;
-    top_surface->set_velocity(Vec3(0, 0, 0.-surface_velocity));
-    std::chrono::duration<double> compaction_time {((h - mat->active_particle_height) / surface_velocity)};
+    top_surface->set_velocity(Vec3(0, 0, 0. - surface_velocity));
+    std::chrono::duration<double> compaction_time{((h - mat->active_particle_height) / surface_velocity)};
     run_for_time.reset(compaction_time);
 
     simulator.run(run_for_time);
     simulator.write_restart_file(output_directory + "/compact_restart_file.res");
 
-    std::cout<<"beginning of unloading"<< std::endl;
+    std::cout << "beginning of unloading" << std::endl;
     top_surface->set_velocity(Vec3(0, 0, surface_velocity));
     //EngineType::SurfaceNormalForceLess zero_force(top_surface, 0.);
 
     simulator.run(max_velocity);
+}
