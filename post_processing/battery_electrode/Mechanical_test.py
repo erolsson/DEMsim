36a19d61b38fb5e0a3274497c3777f5f24251479
elahe <etemadi@kth.se>
2020-11-27 13:00:31 +0100

    "Testing"

---
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -0,0 +1,52 @@
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('classic')
+
+
+def particle_volume():
+    p_volume = 0.0267963
+    return p_volume
+
+
+def dimensions_box(data_directory):
+    with open(data_directory + '/periodic_bc.dou', 'r') as position_data_file:
+        first_line = position_data_file.readlines()[0]
+        first_line = first_line.split(', ')
+
+    data = first_line[:,  3]
+    print(data)
+    return data
+
+
+def box_height(data_directory):
+    with open(data_directory + '/surface_positions.dou', 'r') as position_data_file:
+        first_line = position_data_file.readlines()[0]
+        first_line = first_line.split(', ')
+    height = first_line[:, 34]
+    return height
+
+
+def pressures_box(data_directory):
+    force_data = np.genfromtxt(data_directory + '/force_fabric_tensor.dou', delimiter=', ')
+    with open(data_directory + '/force_fabric_tensor.dou', 'r') as position_data_file:
+        first_line = position_data_file.readlines()[0]
+        first_line = first_line.split(', ')
+
+    surface_tensor = first_line[:, 2]
+    print(surface_tensor)
+    return surface_tensor
+
+
+if __name__ == '__main__':
+    simulation_directory = '../../results/viscoelastic/procent65bt4E34PelleUtanFT/'
+    box_width = 0.172726
+    strain = (box_width - dimensions_box(simulation_directory))/box_width
+    stress = pressures_box(simulation_directory)/(box_width * box_height() * dimensions_box()*2)
+    plt.plot(stress, strain)
+    plt.xlabel("Pressure [Pa]")
+    plt.ylabel("Porosity")
+    plt.show()
