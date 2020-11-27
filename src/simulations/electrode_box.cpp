17e4fdacd048eb4c8805c2f73b1eab68d4f7125a
elahe <etemadi@kth.se>
2020-11-27 15:13:53 +0100

    "Testing"

--- src/simulations/electrode_box.cpp
+++ src/simulations/electrode_box.cpp
@@ -94,7 +94,7 @@ void DEM::electrode_box(const std::string& settings_file_name) {
 
     simulator.add_periodic_boundary_condition('x', -box_side/2, box_side/2);
     simulator.add_periodic_boundary_condition('y', -box_side/2, box_side/2);
-
+    mat-> adhesive = false;
     simulator.set_gravity(Vec3(0, 0, -9.82));
     simulator.set_mass_scale_factor(10.0);
     simulator.setup(1.0*mat->bt);
@@ -137,6 +137,7 @@ void DEM::electrode_box(const std::string& settings_file_name) {
 
     EngineType::RunForTime run_for_time_relax(simulator,15s);
     //simulator.set_rotation(false);
+    mat-> adhesive = true;
     simulator.run(run_for_time_relax);
     simulator.write_restart_file(output_directory + "/relax_restart_file.res");
 
@@ -148,7 +149,7 @@ void DEM::electrode_box(const std::string& settings_file_name) {
     //auto bottom_surface = simulator.get_surface<EngineType::DeformablePointSurfacePointer>("deformable_point_surface_0");
 
     deformable_surface -> set_in_plane_strain_rates(-0.01, 0.);
-
+    mat-> adhesive = true;
     simulator.run(run_for_time_compact_1);
 
     simulator.write_restart_file(output_directory + "/tryck_1.res");
@@ -163,7 +164,7 @@ void DEM::electrode_box(const std::string& settings_file_name) {
     simulator.set_periodic_boundary_condition_strain_rate('x',0.01);
     deformable_surface -> set_in_plane_strain_rates(0.01, 0.);
     EngineType::RunForTime run_for_time_relax_1(simulator,1.75s);
-
+    mat-> adhesive = true;
     simulator.run(run_for_time_relax_1);
     simulator.write_restart_file(output_directory + "/relaxation_1.res");
 
@@ -177,7 +178,7 @@ void DEM::electrode_box(const std::string& settings_file_name) {
     simulator.set_periodic_boundary_condition_strain_rate('x',-0.01);
     deformable_surface -> set_in_plane_strain_rates(-0.01, 0.);
 
-
+    mat-> adhesive = true;
     simulator.run(run_for_time_compact_2);
 
     simulator.write_restart_file(output_directory + "/tryck_2.res");
