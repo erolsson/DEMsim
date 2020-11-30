9efaf2b171d394d606505549832a21a9231c716d
elahe <etemadi@kth.se>
2020-11-30 14:55:26 +0100

    "Testing"

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -51,7 +51,7 @@ def pressures_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/procent70bt4E34height40-100ny/'
+    simulation_directory = '../../results/viscoelastic/procent70bt4E34height40particle100eller/'
 
     volume_box = (2*0.172726)**2 * dimensions_box(simulation_directory)
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
