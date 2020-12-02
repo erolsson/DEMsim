06c7796b64e8d5cdeee05bca7c75315dceb30727
elahe <etemadi@kth.se>
2020-12-02 11:41:37 +0100

    "Testing"

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -26,12 +26,27 @@ def dimensions_box(data_directory):
         wall_data = np.genfromtxt(data_directory + '/surface_positions.dou', delimiter=', ')
         data = np.zeros((wall_data.shape[0], 1))
         data = wall_data[:,  id_idx[0]+32]
+        time= wall_data[:,id_idx[0]+42]
 
         return data
     else:
         raise ValueError("A box could not be defined from the data in " + data_directory +
                          '/surface_positions.dou')
 
+
+def Time(data_directory):
+    with open(data_directory + '/surface_positions.dou', 'r') as position_data_file:
+        first_line = position_data_file.readlines()[0]
+        first_line = first_line.split(', ')
+    id_idx = [i for i in range(len(first_line)) if first_line[i].startswith('ID')]
+    surface_types = [first_line[idx+1][5:] for idx in id_idx]
+    if surface_types.count('ID=') == 0 and surface_types.count('PointSurface') == 2:
+        id_idx.sort(key=lambda x: first_line[x+1])
+        wall_data = np.genfromtxt(data_directory + '/surface_positions.dou', delimiter=', ')
+        time = np.zeros((wall_data.shape[0], 1))
+        time = wall_data[:, id_idx[0]+42]
+
+        return time
 
 def pressures_box(data_directory):
     force_data = np.genfromtxt(data_directory + '/surface_forces.dou', delimiter=', ')
@@ -56,8 +71,14 @@ if __name__ == '__main__':
     volume_box = (2*0.172726)**2 * dimensions_box(simulation_directory)
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
     pressures = pressures_box(simulation_directory)
+    time = Time(simulation_directory)
     plt.plot(pressures, porosity*100)
     plt.xlabel("Pressure [Pa]")
     plt.ylabel("Porosity")
     plt.show()
+    plt.plot(time, pressures )
+    plt.ylabel("Pressure [Pa]")
+    plt.xlabel("time")
+    plt.show()
+
 
