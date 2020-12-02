deb46b2e9acc6ebe2cd057f257b67c722d2ebffd
elahe <etemadi@kth.se>
2020-12-02 12:14:06 +0100

    "Testing"

--- src/simulations/electrode_box.cpp
+++ src/simulations/electrode_box.cpp
@@ -137,12 +137,13 @@ void DEM::electrode_box(const std::string& settings_file_name) {
 
     EngineType::RunForTime run_for_time_relax(simulator,15s);
     //simulator.set_rotation(false);
-    //mat-> adhesive = true;
+    mat-> adhesive = true;
+    top_surface->set_velocity(Vec3(0, 0, surface_velocity));
     simulator.run(run_for_time_relax);
     simulator.write_restart_file(output_directory + "/relax_restart_file.res");
 
     std::cout<<"Biginning of simulation 1"<< std::endl;
-    EngineType::RunForTime run_for_time_compact_1(simulator,1.75s);
+    EngineType::RunForTime run_for_time_compact_1(simulator,5s);
 
 
     simulator.set_periodic_boundary_condition_strain_rate('x',-0.01);
@@ -163,35 +164,14 @@ void DEM::electrode_box(const std::string& settings_file_name) {
 
     simulator.set_periodic_boundary_condition_strain_rate('x',0.01);
     deformable_surface -> set_in_plane_strain_rates(0.01, 0.);
-    EngineType::RunForTime run_for_time_relax_1(simulator,1.75s);
-    //mat-> adhesive = true;
+    EngineType::RunForTime run_for_time_relax_1(simulator,5s);
+    mat-> adhesive = true;
     simulator.run(run_for_time_relax_1);
     simulator.write_restart_file(output_directory + "/relaxation_1.res");
 
 
 
 
-    std::cout<<"Biginning of simulation 2"<< std::endl;
-    EngineType::RunForTime run_for_time_compact_2(simulator,1.9s);
-
-
-    simulator.set_periodic_boundary_condition_strain_rate('x',-0.01);
-    deformable_surface -> set_in_plane_strain_rates(-0.01, 0.);
-
-    //mat-> adhesive = true;
-    simulator.run(run_for_time_compact_2);
-
-    simulator.write_restart_file(output_directory + "/tryck_2.res");
-
-    //unload extra compaction
-
-    std::cout<<"beginning of unloading 2"<< std::endl;
-    simulator.set_periodic_boundary_condition_strain_rate('x',0.01);
-    deformable_surface -> set_in_plane_strain_rates(0.01, 0.);
-    EngineType::RunForTime run_for_time_relax_2(simulator,1.9s);
-    //mat-> adhesive = true;
-    simulator.run(run_for_time_relax_2);
-    simulator.write_restart_file(output_directory + "/relaxation_2.res");
 
 
 
