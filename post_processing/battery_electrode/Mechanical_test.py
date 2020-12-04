fe8cb71d7b24d45af4fab064abcb1ce30d0a7fea
elahe <etemadi@kth.se>
2020-12-04 10:25:51 +0100

    "Testing"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -42,7 +42,7 @@ def time_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/procent70bt4E34height45/'
+    simulation_directory = '../../results/viscoelastic/fixadbugg'
     box_width = 0.172726
     box_height = 0.55
     strain = (box_width - dimensions_box(simulation_directory))[23528:33530]/box_width
