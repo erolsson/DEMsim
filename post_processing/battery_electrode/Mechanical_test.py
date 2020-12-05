6628827bfa67887d0c2792b82741290319e72199
elahe <etemadi@kth.se>
2020-12-05 17:03:10 +0100

    "Testing"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -42,7 +42,7 @@ def time_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = 'C:/DEMsim/results/viscoelastic/fixadbugg'
+    simulation_directory = '../results/viscoelastic/fixadbugg'
     box_width = 0.172726
     box_height = 0.89
     strain = (box_width - dimensions_box(simulation_directory))[128000:141000]/box_width
