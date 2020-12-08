cbae551ffa5f2c1bdfa689415b621e9b5f0e36ae
elahe <etemadi@kth.se>
2020-12-08 14:12:14 +0100

    "relaxation"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -45,10 +45,10 @@ if __name__ == '__main__':
     simulation_directory = 'C:/DEMsim/results/viscoelastic/100-relaxation'
     box_width = 0.172726
     box_height = 0.6
-    strain = (box_width - dimensions_box(simulation_directory))[:]/box_width
+    strain = (box_width - dimensions_box(simulation_directory))[101767:111769]/box_width
     Stress = pressures_box(simulation_directory)/(box_width * box_height * box_width *2)
-    stress = pressures_box(simulation_directory)[:]/(box_width * box_height *
-                                                               dimensions_box(simulation_directory)[:] *2)
+    stress = pressures_box(simulation_directory)[101767:111769]/(box_width * box_height *
+                                                               dimensions_box(simulation_directory)[101767:111769] *2)
     #inkompresibelt på binder, isotropiskt material
     # inelastic strain
     time = time_box(simulation_directory)
