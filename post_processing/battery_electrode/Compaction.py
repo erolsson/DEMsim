5b667bce27c65685bdeb1c7eec220af6727ac230
elahe <etemadi@kth.se>
2020-12-10 09:21:32 +0100

    "Fixed _restart"

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -67,7 +67,7 @@ def pressures_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/new_porosity'
+    simulation_directory = '../../results/viscoelastic/100-relaxation_tat'
 
     volume_box = (0.345452)**2 * dimensions_box(simulation_directory)[:]
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
