ab1d122e3c086b379f8463d7000cc8b7e5a3e0d3
elahe <etemadi@kth.se>
2020-12-12 13:15:55 +0100

    "Chcek_tensor_force"

--- src/simulations/cathode_mechanical_simulations.cpp
+++ src/simulations/cathode_mechanical_simulations.cpp
@@ -44,25 +44,36 @@ void DEM::Cathode_mechanical_simulations(const std::string &settings_file_name) 
     //simulator.write_restart_file(output_directory + "/stability_check.res");
 
     double surface_velocity = 0.01;
-    top_surface->set_velocity(Vec3(0, 0,-surface_velocity));
-    EngineType::RunForTime run_for_time_compact(simulator,50s);
-    simulator.run(run_for_time_compact);
-    //simulator.write_restart_file(output_directory + "/minimum_height.res");
 
 
-
-    std::cout<<"beginning of unloading"<< std::endl;
-    top_surface->set_velocity(Vec3(0, 0, surface_velocity));
-    EngineType::SurfaceNormalForceLess zero_force(top_surface, 0.);
-    // simulator.set_rotation(false);
-    simulator.run(zero_force);
-
-    EngineType::RunForTime run_for_time_relax(simulator,5s);
+    EngineType::RunForTime run_for_time_relax(simulator,15s);
     //simulator.set_rotation(false);
    // mat-> adhesive = true;
     top_surface->set_velocity(Vec3(0, 0, surface_velocity));
     simulator.run(run_for_time_relax);
-    simulator.write_restart_file(output_directory + "/new_porosity.res");
+    //simulator.write_restart_file(output_directory + "/new_porosity.res");
+
+    std::cout<<"Biginning of simulation 4"<< std::endl;
+    EngineType::RunForTime run_for_time_compact_4(simulator,0.003s);
+
+    simulator.set_periodic_boundary_condition_strain_rate('x',-1.0);
+    deformable_surface -> set_in_plane_strain_rates(-1.0, 0.);
+    //simulator.set_mass_scale_factor(10.0);
+    mat-> adhesive = true;
+    simulator.run(run_for_time_compact_4);
+
+    simulator.write_restart_file(output_directory + "/tryck_4.res");
+
+    //unload extra compaction
+
+    std::cout<<"beginning of unloading 4"<< std::endl;
+    simulator.set_periodic_boundary_condition_strain_rate('x',0.);
+    deformable_surface -> set_in_plane_strain_rates(0., 0.);
+    EngineType::RunForTime run_for_time_relax_4(simulator,50s);
+    //simulator.set_mass_scale_factor(1.0);
+    mat-> adhesive = true;
+    simulator.run(run_for_time_relax_4);
+    simulator.write_restart_file(output_directory + "/relaxation_4.res");
 
 
 
