f4e6fbc1e81adb1d3905c98c8b532a8bd2ca74e5
elahe <etemadi@kth.se>
2020-12-12 17:57:43 +0100

    "Chcek_tensor_force"

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -67,7 +67,7 @@ def pressures_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/porosity_53relaxation_test/fixar_porosity/'
+    simulation_directory = '../../resultsviscoelastic/porosity_53mechanical_test/'
 
     volume_box = (0.345452)**2 * dimensions_box(simulation_directory)[:]
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
