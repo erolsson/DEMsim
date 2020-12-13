8b520ed38375cd631fa4243e8b3639f7f3437d0f
elahe <etemadi@kth.se>
2020-12-13 09:00:36 +0100

    "Chcek_tensor_force"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -42,7 +42,7 @@ def time_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/100-relaxation_test/'
+    simulation_directory = '../../results/viscoelastic/00-mechanical_tes/'
     box_width = 0.172726
     box_height = 0.733144
     strain = (box_width - dimensions_box(simulation_directory))[:]/box_width
