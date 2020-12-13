3fe8a50a344545d85c2e4898357ddcc21db07bf2
elahe <etemadi@kth.se>
2020-12-13 15:59:27 +0100

    "Chcek_tensor_force"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -19,6 +19,21 @@ def dimensions_box(data_directory):
     return data
 
 
+def position_zz(data_directory):
+    with open(data_directory + '/surface_positions.dou', 'r') as periodic_bc:
+        first_line = periodic_bc.readlines()[0]
+        first_line = first_line.split(', ')
+    id_idx = [i for i in range(len(first_line)) if first_line[i].startswith('ID')]
+    surface_types = [first_line[idx+1][5:] for idx in id_idx]
+    if surface_types.count('ID=') == 0 and surface_types.count('PointSurface') == 2:
+        id_idx.sort(key=lambda x: first_line[x+1])
+        wall_data = np.genfromtxt(data_directory + '/surface_positions.dou', delimiter=', ')
+        time = np.zeros((wall_data.shape[0], 1))
+        time = wall_data[:, id_idx[0]+42]
+
+        return time
+
+
 def pressures_box(data_directory):
     with open(data_directory + '/force_fabric_tensor.dou', 'r') as force_fabric_tensor:
         first_line = force_fabric_tensor.readlines()[0]
@@ -29,6 +44,15 @@ def pressures_box(data_directory):
     force = force_data[:,  id_idx[0]+1]
     return force
 
+def pressures_box_yy(data_directory):
+    with open(data_directory + '/force_fabric_tensor.dou', 'r') as force_fabric_tensor:
+        first_line = force_fabric_tensor.readlines()[0]
+        first_line = first_line.split(', ')
+    id_idx = [i for i in range(len(first_line))]
+    force_data = np.genfromtxt(data_directory + '/force_fabric_tensor.dou', delimiter=', ')
+    force = np.zeros((force_data.shape[0], 1))
+    force = force_data[:,  id_idx[0]+4]
+    return force
 
 def time_box(data_directory):
     with open(data_directory + '/force_fabric_tensor.dou', 'r') as force_fabric_tensor:
@@ -45,12 +69,17 @@ if __name__ == '__main__':
     simulation_directory = '../../results/viscoelastic/100-mechanical_test/'
     box_width = 0.212311
     box_height = 0.528
-    strain = (box_width - dimensions_box(simulation_directory))[283631:303631]/box_width
+    E = 1e9
+    strain = (box_width - dimensions_box(simulation_directory))[:]/box_width
     Stress = pressures_box(simulation_directory)[:]/(box_width * box_height * box_width *2)
-    stress = pressures_box(simulation_directory)[283631:303631]/(box_width * box_height *
-                                                               dimensions_box(simulation_directory)[283631:303631] *2)
+    Stress_y = pressures_box_yy(simulation_directory)[:]/(box_width * box_height * box_width *2)
+    stress = pressures_box(simulation_directory)[:]/(box_width * box_height *
+                                                               dimensions_box(simulation_directory)[:] *2)
+
     #inkompresibelt på binder, isotropiskt material
     # inelastic strain
+    epsilon_zz = position_zz(simulation_directory)[:]-box_height
+    nu = -E*epsilon_zz(simulation_directory)[:]/(Stress*Stress_y)
     time = time_box(simulation_directory)[:]
     plt.plot(time, Stress)
     plt.xlabel("time[s]")
@@ -60,4 +89,9 @@ if __name__ == '__main__':
     plt.xlabel("Strain")
     plt.ylabel("Stress [Pa]")
     plt.show()
+    plt.plot(time, nu)
+    plt.xlabel("Strain")
+    plt.ylabel("Stress [Pa]")
+
+
 
