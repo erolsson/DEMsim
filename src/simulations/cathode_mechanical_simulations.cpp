0a2c137657daa27ffa760b88bc3583e6b00f161d
elahe <etemadi@kth.se>
2020-12-18 11:24:02 +0100

    "relaxation"

--- src/simulations/cathode_mechanical_simulations.cpp
+++ src/simulations/cathode_mechanical_simulations.cpp
@@ -67,8 +67,8 @@ void DEM::Cathode_mechanical_simulations(const std::string &settings_file_name) 
     //unload extra compaction
 
     std::cout<<"beginning of unloading 4"<< std::endl;
-    simulator.set_periodic_boundary_condition_strain_rate('x',-0.01);
-    deformable_surface -> set_in_plane_strain_rates(-0.01, 0.);
+    simulator.set_periodic_boundary_condition_strain_rate('x',0.0);
+    deformable_surface -> set_in_plane_strain_rates(0.0, 0.);
     EngineType::RunForTime run_for_time_relax_2(simulator,240.0s);
     //simulator.set_mass_scale_factor(1.0);
     mat-> adhesive = true;
