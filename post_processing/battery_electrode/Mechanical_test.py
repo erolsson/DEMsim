9fa30f3b2d7860b2dae0f7353c3d19ba4fc7a6e2
elahe <etemadi@kth.se>
2021-01-10 19:11:49 +0100

    "Plot-results"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -67,30 +67,30 @@ def time_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/More-compact-tryck/'
-    box_width = 0.726136 *2
-    surface_height = 1.70 # when the mechanical testing begins
+    simulation_directory = '../../results/viscoelastic/nya_radiii/'
+    box_width = 0.580403 *2
+    surface_height = 1.197 # when the mechanical testing begins
     E = 2e9
     print(dimensions_box(simulation_directory))
-    strain = -( 2* dimensions_box(simulation_directory)[10749:12656]-box_width)/box_width
-    Stress = pressures_box(simulation_directory)[10749:12656]/(box_width * surface_height * box_width *2)
-    Stress_y = pressures_box_yy(simulation_directory)[10749:12656]/(box_width * position_zz(simulation_directory)[10749:12656] *
-                                                                   dimensions_box(simulation_directory)[10749:12656] *2)
+    strain = -( 2* dimensions_box(simulation_directory)[15465:17372]-box_width)/box_width
+    Stress = pressures_box(simulation_directory)[15465:17372]/(box_width * surface_height * box_width *2)
+    Stress_y = pressures_box_yy(simulation_directory)[15465:17372]/(box_width * position_zz(simulation_directory)[15465:17372] *
+                                                                   dimensions_box(simulation_directory)[15465:17372] *2)
 
-    stress = pressures_box(simulation_directory)[10749:12656]/(box_width * position_zz(simulation_directory)[10749:12656] *
-                                                              dimensions_box(simulation_directory)[10749:12656] *2)
+    stress = pressures_box(simulation_directory)[15465:17372]/(box_width * position_zz(simulation_directory)[15465:17372] *
+                                                              dimensions_box(simulation_directory)[15465:17372] *2)
 
 
     #inkompresibelt på binder, isotropiskt material+
     # inelastic strain
-    epsilon_zz = -(position_zz(simulation_directory)[10749:12656]-surface_height)/surface_height
-    print(position_zz(simulation_directory)[10749:12656])
+    epsilon_zz = -(position_zz(simulation_directory)[15465:17372]-surface_height)/surface_height
+    print(position_zz(simulation_directory)[15465:17372])
     print(epsilon_zz)
     total_stress = Stress+Stress_y
     nu = -(E*epsilon_zz)/ total_stress
     print(nu)
 
-    time = time_box(simulation_directory)[10749:12656]
+    time = time_box(simulation_directory)[15465:17372]
     plt.plot(time, stress)
     plt.xlabel("time[s]")
     plt.ylabel("Stress [Pa]")
