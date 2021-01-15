bbce8c693ba6d6d19f1c19808fa85f3db156258a
elahe <etemadi@kth.se>
2021-01-15 13:21:27 +0100

    "fixing50%porosity"

--- post_processing/battery_electrode/Mechanical_test.py
+++ post_processing/battery_electrode/Mechanical_test.py
@@ -67,30 +67,30 @@ def time_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/81height3bt/'
+    simulation_directory = '../../results/viscoelastic/81height4bt/'
     box_width = 0.726136 *2
     surface_height = 1.197 # when the mechanical testing begins
     E = 2e9
     print(dimensions_box(simulation_directory))
-    strain = -( 2* dimensions_box(simulation_directory)[25950:28187]-box_width)/box_width
-    Stress = pressures_box(simulation_directory)[25950:28187]/(box_width * surface_height * box_width *2)
-    Stress_y = pressures_box_yy(simulation_directory)[25950:28187]/(box_width * position_zz(simulation_directory)[25950:28187] *
-                                                                   dimensions_box(simulation_directory)[25950:28187] *2)
+    strain = -( 2* dimensions_box(simulation_directory)[26356:28928]-box_width)/box_width
+    Stress = pressures_box(simulation_directory)[26356:28928]/(box_width * surface_height * box_width *2)
+    Stress_y = pressures_box_yy(simulation_directory)[26356:28928]/(box_width * position_zz(simulation_directory)[26356:28928] *
+                                                                   dimensions_box(simulation_directory)[26356:28928] *2)
 
-    stress = pressures_box(simulation_directory)[25950:28187]/(box_width * position_zz(simulation_directory)[25950:28187] *
-                                                              dimensions_box(simulation_directory)[25950:28187] *2)
+    stress = pressures_box(simulation_directory)[26356:28928]/(box_width * position_zz(simulation_directory)[26356:28928] *
+                                                              dimensions_box(simulation_directory)[26356:28928] *2)
 
 
     #inkompresibelt på binder, isotropiskt material+
     # inelastic strain
-    epsilon_zz = -(position_zz(simulation_directory)[25950:28187]-surface_height)/surface_height
-    print(position_zz(simulation_directory)[25950:28187])
+    epsilon_zz = -(position_zz(simulation_directory)[26356:28928]-surface_height)/surface_height
+    print(position_zz(simulation_directory)[26356:28928])
     print(epsilon_zz)
     total_stress = Stress+Stress_y
     nu = -(E*epsilon_zz)/ total_stress
     print(nu)
 
-    time = time_box(simulation_directory)[25950:28187]
+    time = time_box(simulation_directory)[26356:28928]
     plt.plot(time, stress)
     plt.xlabel("time[s]")
     plt.ylabel("Stress [Pa]")
