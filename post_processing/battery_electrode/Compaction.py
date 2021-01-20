5cbe1984b1d1044b06e92e7eaf1e765ec006df75
elahe <etemadi@kth.se>
2021-01-20 15:25:32 +0100

    100

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -61,15 +61,15 @@ def pressures_box(data_directory):
 
     surface1_force = force_data[:, surface_indices[0]+6]
 
-    p = surface1_force/(0.156231*0.156231)
+    p = surface1_force/(0.580403*0.580403)
 
     return p
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/68hbt2-100/'
+    simulation_directory = '../../results/viscoelastic/0.580403'
 
-    volume_box = (0.156231*2)**2 * dimensions_box(simulation_directory)[:]
+    volume_box = (0.580403*2)**2 * dimensions_box(simulation_directory)[:]
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
     pressures = pressures_box(simulation_directory)[:]
     time = Time(simulation_directory)[:]
