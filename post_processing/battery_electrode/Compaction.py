26ede022d0e1117fd68dc6f9b8e0aa12c769babe
elahe <etemadi@kth.se>
2021-01-22 11:34:03 +0100

    100

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -67,7 +67,7 @@ def pressures_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/fixing_porosity/utan-binder-ny/'
+    simulation_directory = '../../results/viscoelastic/utan-binder-ny/'
 
     volume_box = (0.172726*2)**2 * dimensions_box(simulation_directory)[:]
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
