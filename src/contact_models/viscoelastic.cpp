09d19ef5ad579eb344dd685f44d68f46ad6fcc5a
elahe <etemadi@kth.se>
2021-02-03 11:10:21 +0100

    testing bonded

--- src/contact_models/viscoelastic.cpp
+++ src/contact_models/viscoelastic.cpp
@@ -249,7 +249,7 @@ double DEM::Viscoelastic::update_normal_force(double h)
 
             F_visc += kB_*dF;
         }
-        else if (h < -bt_ && !bonded_){
+        else {
             F_visc = 0;
             for (unsigned i = 0; i != M; ++i) {
                 di_[i] = 0;
