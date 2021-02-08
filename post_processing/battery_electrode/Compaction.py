dc42efb5ac067f47b0846705a3f7b117ffd1ba67
elahe <etemadi@kth.se>
2021-02-08 08:40:52 +0100

    Fixing porosity

--- post_processing/battery_electrode/Compaction.py
+++ post_processing/battery_electrode/Compaction.py
@@ -66,7 +66,7 @@ def pressures_box(data_directory):
 
 
 if __name__ == '__main__':
-    simulation_directory = '../../results/viscoelastic/cubic_box-3200_h85/'
+    simulation_directory = '../../results/viscoelastic/cubic_box-3200_h89/'
 
     volume_box = (0.448928*2)**2 * dimensions_box(simulation_directory)[:]
     porosity = (1-(particle_volume()*(1+0.07/(0.33+0.07)))/volume_box)
