6efc98963635381fe4810cc296b34dba0d3ed32a
elahe <etemadi@kth.se>
2021-04-12 16:32:17 +0200

    Testing different parameters

--- post_processing/battery_electrode/porosity.py
+++ post_processing/battery_electrode/porosity.py
@@ -33,7 +33,7 @@ def calculate_occupied_volume(time, simulation_directory):
 
 
 def main():
-    time_for_test = 14.7
+    time_for_test = 3.22305
     directory = os.path.expanduser('/scratch/users/elaheh/DEMsim/results/viscoelastic/mechanical-test-experimental-5-cykles-E34bt01Rbr05/')
     box_edges = np.genfromtxt(directory + 'periodic_bc.dou', delimiter=',')
     time = box_edges[:, 0]
