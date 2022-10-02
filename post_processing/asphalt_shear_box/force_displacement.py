6c402d00390db1a0cd161808e01009c1d6ab3d3d
erolsson@kth.se <erolsson@kth.se>
2022-10-02 21:34:31 +0200

    "Commenting the file close_die_compaction.cpp to document a simulation"

--- post_processing/asphalt_shear_box/force_displacement.py
+++ post_processing/asphalt_shear_box/force_displacement.py
@@ -19,14 +19,19 @@ box = ax.get_position()
 ax.set_position([0.1, 0.15, 0.55, box.height])
 
 main_directory = pathlib.Path("~/DEMsim/results/asphalt_shear_box/bonded_plane_wall_friction_2/").expanduser()
+exp_directory = pathlib.Path("~/asphalt_bond_strength/experiments/bonded").expanduser()
 for simulation, c in zip(["Small_Small", "Big_Small", "Big_Big"], ['g', 'r', 'b', 'm']):
-    for p, line in zip(["100kPa", "400kPa"], ['--', '-']):
+    for fig, p in enumerate(["100kPa", "400kPa"]):
+        plt.figure(fig)
         simulations = [1, 2, 3]
+        data = np.genfromtxt(exp_directory / (simulation.lower() + "_" + p + ".dat"))
+        plt.plot(data[:, 0], data[:, 1], c, lw=3)
         for sim in simulations:
             directory = main_directory / str(sim) / (simulation.lower() + "_" + p)/"shear_test"
             surface_forces = np.genfromtxt(directory / "surface_forces.dou", delimiter=",")
             surface_positions = np.genfromtxt(directory / "surface_positions.dou", delimiter=",")
             kinetic_energy = np.genfromtxt(directory / "kinetic_energy.dou", delimiter=",")
+            plt.plot(surface_positions[:, -15], surface_forces[:, -4], c + '--')
             if sim == simulations[0]:
                 d = surface_positions[:, -15]
                 f = -surface_forces[:, -4]
@@ -37,25 +42,16 @@ for simulation, c in zip(["Small_Small", "Big_Small", "Big_Big"], ['g', 'r', 'b'
             else:
                 d += surface_positions[:, -15]
                 f += -surface_forces[:, -4]
-        if line == '-':
-            label = simulation.replace('_', '-')
-        else:
-            label = None
 
         plt.figure(0)
-        plt.plot(d*1000/len(simulations), -f/1000/len(simulations), c + line, lw=2, label=label)
-plt.figure(0)
-plt.ylim(0, 15)
-plt.xlim(0, 8)
+        plt.plot(d*1000/len(simulations), -f/1000/len(simulations), c + '--', lw=3, label=simulation.replace('_', '-'))
 
-plt.plot([-1, -2], [-1, -2], 'w', label='white')
-plt.plot([-1, -2], [-1, -2], '--k', lw=2, label='100 kPa')
-plt.plot([-1, -2], [-1, -2], '-k', lw=2, label='400 kPa')
-plt.ylim(0)
-legend = ax.legend(loc='upper left', bbox_to_anchor=(1., 1.035), numpoints=1)
-legend.get_texts()[3].set_color("white")
-plt.gca().add_artist(legend)
-plt.xlabel("Displacement [mm]")
-plt.ylabel("Force [kN]")
-plt.savefig("bonded.png")
+for fig in range(2):
+    plt.figure(fig)
+    plt.ylim(0, 15)
+    plt.xlim(0, 8)
+
+    legend = ax.legend(loc='upper left', bbox_to_anchor=(1., 1.035), numpoints=1)
+    plt.xlabel("Displacement [mm]")
+    plt.ylabel("Force [kN]")
 plt.show()
