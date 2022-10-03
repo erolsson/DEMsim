d40c3d69d2730c0a6b137293ec10426c8a89c301
erolsson@kth.se <erolsson@kth.se>
2022-10-03 12:08:46 +0200

    "Commenting the file close_die_compaction.cpp to document a simulation"

--- post_processing/asphalt_shear_box/force_displacement_unbonded.py
+++ post_processing/asphalt_shear_box/force_displacement_unbonded.py
@@ -45,7 +45,7 @@ for fig_number, p in enumerate(["100kPa", "400kPa"]):
                 f += -surface_forces[:, -4]
 
         plt.plot(d*1000/len(simulations), uniform_filter1d(f, size=200)/1000/len(simulations), c + '--', lw=3)
-        plt.text(0.2, 0.5, r"\bf{" + p.replace("kPa", " kPa") + "}",
+        plt.text(0.5, 0.2, r"\bf{" + p.replace("kPa", " kPa") + "}",
                  horizontalalignment='center',
                  verticalalignment='center',
                  transform=ax.transAxes)
