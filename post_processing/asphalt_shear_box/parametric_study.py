2e8de0404a4e4499c9efd6c4ebd124275d3ed5b6
erolsson@kth.se <erolsson@kth.se>
2022-10-06 22:29:37 +0200

    "Commenting the file close_die_compaction.cpp to document a simulation"

--- post_processing/asphalt_shear_box/parametric_study.py
+++ post_processing/asphalt_shear_box/parametric_study.py
@@ -33,7 +33,7 @@ for p, color in zip(pressures, 'br'):
             directory = (main_directory / str(sim) / (str(config[0]) + "_" + str(config[1]) + "_" + str(p) + "kPa")
                          / "shear_test")
             surface_forces = np.genfromtxt(directory / "surface_forces.dou", delimiter=",")
-            max_f[i, j] = np.max(surface_forces[:, -4])/1e3
+            max_f[i, j] = np.max(surface_forces[:, -4])
     plt.plot(size_ratio, np.mean(max_f, axis=1)/area, '-' + color, lw=3, label=str(p) + " kPa")
     plt.errorbar(size_ratio, np.mean(max_f, axis=1)/area, np.std(max_f, axis=1), fmt="none", elinewidth=2,
                  ecolor=color)
@@ -43,7 +43,7 @@ for p, color in zip(pressures, 'br'):
         for j, sim in enumerate(simulations):
             directory = main_directory / str(sim) / (simulation.lower() + "_" + str(p) + "kPa") / "shear_test"
             surface_forces = np.genfromtxt(directory / "surface_forces.dou", delimiter=",")
-            max_f[j] = np.max(surface_forces[:, -4])/1e3
+            max_f[j] = np.max(surface_forces[:, -4])
         ratio = 1 if simulation != "Big_Small" else 9.5/5.5
         plt.plot(ratio, np.mean(max_f)/area, symbol + color, lw=3, mew=3, ms=12)
         plt.errorbar(ratio, np.mean(max_f)/area, np.std(max_f), fmt="none", elinewidth=2,
@@ -55,7 +55,7 @@ plt.plot([-1, -2], [1, 1], 'w', label="white")
 for simulation, symbol in zip(["(5.5/5.5)", "(5.5/9.5)", "(9.5/9.5)"], ['o', 'x', 's']):
     plt.plot([-2], [1], 'k' + symbol, ms=12, mew=3, mfc='w', label=simulation.replace('_', '-'))
 plt.xlabel("Size ratio $D_2/D_1$ [-]", fontsize=24)
-plt.ylabel("Maximum force [kN]", fontsize=24)
+plt.ylabel("Maximum shear stress [MPa]", fontsize=24)
 legend = ax.legend(loc='upper left', bbox_to_anchor=(1., 1.035), numpoints=1)
 legend.get_texts()[2].set_color("white")
 plt.savefig("parametric_study.png")
