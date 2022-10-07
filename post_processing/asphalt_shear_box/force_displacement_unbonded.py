2c51b1c96e69ae46f0af08b1dc3839b18feeb9be
erolsson@kth.se <erolsson@kth.se>
2022-10-07 15:14:21 +0200

    "Commenting the file close_die_compaction.cpp to document a simulation"

--- post_processing/asphalt_shear_box/force_displacement_unbonded.py
+++ post_processing/asphalt_shear_box/force_displacement_unbonded.py
@@ -31,7 +31,7 @@ ax.set_position([0.1*11/8, 0.15, 0.55*11/8, box.height])
 
 area = np.pi*50*50
 sizes = {"Small_Small": "(5.5/5.5)", "Big_Small": "(5.5/9.5)", "Big_Big": "(9.5/9.5)"}
-for fig_number, (p, fig_label) in enumerate(zip(["100kPa", "400kPa"], ["(a)", "(b)"])):
+for fig_number, (p, fig_label) in enumerate(zip(["100kPa", "400kPa"], ["a", "b"])):
     plt.figure(fig_number)
     ax = plt.subplot(111)
     for simulation, c in zip(sizes.keys(), ['g', 'r', 'b', 'm']):
